f8ce77793baebd7f4685ba099834fa5fc8751e7f
gouzidev <gouzi.dev@gmail.com>
2023-03-16 06:57:25 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
-*main - entry point
-*return: always 0 (success)
-*/
+ * main - entry point
+ *
+ * Return: always 0
+ */
 int main(void)
 {	
 	puts("\"Programming is like building a multilingual puzzle");
