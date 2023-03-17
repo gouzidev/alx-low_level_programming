43a52b12e044a76d111ba801319bff3c94830f98
gouzidev <gouzi.dev@gmail.com>
2023-03-17 16:34:07 -0700

    new

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,7 +11,7 @@
 */
 int main(void)
 {
-	char alphabets[] = "abcdefghijklmnopqrstuvwxyz\n";
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
 	int i = 0;
 	int len = sizeof(alphabets) / sizeof(alphabets[0]);
 
