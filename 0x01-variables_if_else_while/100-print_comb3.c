6199b011bceb1a9df47a37093f2fe520aca428c1
gouzidev <gouzi.dev@gmail.com>
2023-03-17 18:56:42 -0700

    new

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,9 +7,11 @@
 
 int main(void)
 {
-	for (int i = 0; i <= 9 ; i++)
+	int i, j = 0;
+
+	for (i = 0; i <= 9 ; i++)
 	{
-		for (int j = 0; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			if (i != j && j > i)
 			{
