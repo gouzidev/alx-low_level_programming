a4a710f9afc7eca9b731330da82664f00c1daaac
gouzidev <gouzi.dev@gmail.com>
2023-03-21 13:11:05 -0700

    new

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,11 @@
-int printer(char str[])
+int printer(char* str)
 {
 	int i = 0;
 
         while (str[i] =! '\0')
 	{
                 putchar(str[i]);
-        }
+		i++;
+	}
         return (0);
 }
