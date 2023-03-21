4c452d461af4d8debd23828263e63ac613a45e03
gouzidev <gouzi.dev@gmail.com>
2023-03-21 15:47:41 -0700

    new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,12 @@
 #include "main.h"
+
+/**
+ * print_alphabet - Print alphabet in lowercase using only `_putchar`
+ * Return: 0
+ */
+
 void print_alphabet(void)
 {
-	int ch = 97;
-
 	while (ch < 123)
 	{
 		_putchar(ch);
