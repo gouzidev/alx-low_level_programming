8ddafd90f5f5c646992ff8d0c1477510cfb8461b
gouzidev <gouzi.dev@gmail.com>
2023-03-21 15:19:54 -0700

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
 int main(void)
 {
 	char str[] = "_putchar";
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
-void _putchar(char c)
+int _putchar(char c)
 {
   putchar(c);
-  return;
+  return (0);
 }
