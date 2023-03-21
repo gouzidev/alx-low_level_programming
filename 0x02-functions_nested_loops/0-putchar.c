9fbeb55439fa0efd423bf5a9ec5bc7aeb0788f51
gouzidev <gouzi.dev@gmail.com>
2023-03-21 12:51:40 -0700

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,10 +12,15 @@ int main(void)
 	printer("_putchar");
 	return (0);
 }
-
+/**
+* printer - prints my strings
+* Description: displays str using for loop and putchar
+* #str: string to give the function
+* Return: 0
+**/
 int printer(char *str)
 {
-	for(int i = 0; i < sizeof(str)/sizeof(str[0]); i++)
+	for (int i = 0; i < sizeof(str) / sizeof(str[0]); i++)
 	{
 		putchar(str[i]);
 	}
