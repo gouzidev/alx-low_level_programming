4881598e4285e3ed6d0a05583b97302464b920ff
gouzidev <gouzi.dev@gmail.com>
2023-03-21 14:26:16 -0700

    new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,8 +10,3 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
-/**
-* printer - prints my strings
-* Description: displays str using for loop and putchar
-* #str: string to give the function
-**/
