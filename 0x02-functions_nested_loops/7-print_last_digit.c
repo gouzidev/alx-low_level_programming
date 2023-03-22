f8c68217701a4526b607ecea9aaba5d9090c2775
gouzidev <gouzi.dev@gmail.com>
2023-03-21 17:39:55 -0700

    new

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,17 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
-  * print_last_digit -  function that checks for lowercase character.
-  * @n: int type letter
-  * Return: 1 or 0
+  * print_last_digit -  function that prints the last digit of a number..
+  *  @n: int type number
+  * Return: number
   */
 
 
 int print_last_digit(int n)
 {
-	printf("%i", ((abs(n) % 10) * 11));
-	return ((abs(n) % 10) * 11);
+	int r = n % 10;
+
+	if (n < 0)
+		r = -r;
+	_putchar(r + '0');
+	return (r);
 }
