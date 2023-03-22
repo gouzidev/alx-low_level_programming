c32b3f47f20c472ab2b918bf1e76f91ad3a58f9b
gouzidev <gouzi.dev@gmail.com>
2023-03-21 17:08:40 -0700

    new

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * _abs -  function that checks for lowercase character.
+  * @n: int type letter
+  * Return: 1 or 0
+  */
+
+
+int _abs (int n)
+{
+	return abs(n);
+}
