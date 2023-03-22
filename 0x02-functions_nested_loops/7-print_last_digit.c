ce713d5306066c8e6144f6fb1d51e059bff9dd3a
gouzidev <gouzi.dev@gmail.com>
2023-03-21 17:15:29 -0700

    new

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * print_last_digit -  function that checks for lowercase character.
+  * @n: int type letter
+  * Return: 1 or 0
+  */
+
+
+int print_last_digit(int n)
+{
+	return (abs(n)) % 10;
+}
