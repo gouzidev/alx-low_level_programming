fd3d6c91d4e54b73e154760bf9cdef4a35b3503c
gouzidev <gouzi.dev@gmail.com>
2023-03-23 07:51:22 -0700

    new

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - oco
+ * Return: void
+**/
+
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		if ( i != 50 || i != 52)
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar(10);
+}
