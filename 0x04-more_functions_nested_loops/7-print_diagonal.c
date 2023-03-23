5caf1b5151ecc184fa87e16af27e43061db2aa64
gouzidev <gouzi.dev@gmail.com>
2023-03-23 16:15:46 -0700

    new

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - oco
+ * Description: Uses stuf
+ * Return: void. no return
+**/
+
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 0;
+
+
+
+	while (i < n)
+	{
+		j = 0;
+		while (j > i)
+		{
+			if (j < i)
+			{
+				_putchar(' ');
+			}
+			j++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+}
