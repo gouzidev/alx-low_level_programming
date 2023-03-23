9690a978f99c05ac4cfa5b427f3c9fe623d38331
gouzidev <gouzi.dev@gmail.com>
2023-03-23 07:27:00 -0700

    new

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - oco
+ * @a: the character
+ * @b: the character
+ * Return: 0
+**/
+
+void print_numbers(void)
+{
+	int i = 48;
+	
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar(10);
+}
