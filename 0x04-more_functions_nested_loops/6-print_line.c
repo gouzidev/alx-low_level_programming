6db4c01d9f9983621efb71119e6b5858f5fe54f3
gouzidev <gouzi.dev@gmail.com>
2023-03-23 16:02:59 -0700

    new

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_lines - oco
+ * @n: number of lines in int
+ * Description: Uses stuf
+ * Return: void. no return
+**/
+
+void print_lines(int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
