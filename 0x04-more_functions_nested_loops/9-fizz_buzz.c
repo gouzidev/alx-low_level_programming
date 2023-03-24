1697d34caf05351bcf51b5c442ae48e83cdcb4a1
gouzidev <gouzi.dev@gmail.com>
2023-03-23 17:33:23 -0700

    new

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,29 +10,20 @@ int main(void)
 {
 	int i;
 
-	i = 1;
-	while (i <= 100)
+	for (i = 1; i <= 100; i++)
 	{
 		if (1 % 3 == 0 && i % 5 == 0)
-		{
 			printf("FizzBuzz");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz");
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz");
-		}
 		else
-		{
 			printf("%d", i);
-		}
 		if (i != 100)
 			printf(" ");
 		i++;
 	}
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
