9a89f5ac603d4d76b061fe04e8933e8cabf29694
gouzidev <gouzi.dev@gmail.com>
2023-03-27 15:38:51 -0700

    new

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - oco
+ * @a : pointer
+ * @b : pointer
+ * Description: Uses stuf
+ * Return: void. no return
+**/
+
+void swap_int(int *a, int *b)
+{
+	int* c = a;
+
+	a = b;
+	b = c;
+}
