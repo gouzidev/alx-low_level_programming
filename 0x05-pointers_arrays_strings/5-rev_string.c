bca1d3b840263c7d24b9d3a321e60795127d5561
gouzidev <gouzi.dev@gmail.com>
2023-03-27 16:34:09 -0700

    new

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - oco
+ * @s : pointer
+ * Description: Uses stuf
+ * Return: void.
+**/
+
+void rev_string(char *s)
+{
+	int lenS = strlen(s);
+	int i;
+	char *newStr[strlen(e)];
+	int j = 0;
+
+	for (i = (lenS - 1); i >= 0; i--)
+	{
+		newStr[i] = s[i];
+		j++;
+	}
+	s = newStr;
+}
