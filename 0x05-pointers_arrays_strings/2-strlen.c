1589f99825347efd5de8a2e040f7b39462f8c0b7
gouzidev <gouzi.dev@gmail.com>
2023-03-27 16:01:09 -0700

    new

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - oco
+ * @s : pointer
+ * Description: Uses stuf
+ * Return: int.
+**/
+
+void _strlen(char *s)
+{
+	return strlen(s);
+}
