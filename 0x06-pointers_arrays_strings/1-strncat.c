763eb2dd06976cd306e690b80380adecc8a4619a
gouzidev <gouzi.dev@gmail.com>
2023-03-30 08:11:29 -0700

    new

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,31 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * *_strncat - appends the src string to the dest string
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
  *
- * @dest: char to check
- * @src: char to check
- * @n: number of bytes from src
- * Description: This will append the src string to the dest string
- * Return: a pointer to the resulting string dest
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int a, b, largosrc;
+	int i;
+	int j;
 
-	for (a = 0; dest[a] != '\0'; a++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
+		i++;
 	}
-	for (largosrc = 0; src[largosrc] != 0; largosrc++)
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
+	dest[i] = src[j];
+	i++;
+	j++;
 	}
-	for (b = 0; b < n && b < largosrc; b++, a++)
-	{
-		dest[a] = src[b];
-	}
+	dest[i] = '\0';
 	return (dest);
 }
+
