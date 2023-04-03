951461acdb47df5a220b8fb3219c9f5158709ba5
gouzidev <gouzi.dev@gmail.com>
2023-04-03 15:42:38 -0700

    new

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s : string
+ * @c: character
+ * Return: NULL
+ */
+
+
+char *_strchr(char *s, char c)
+{
+	if (s == NULL)
+	{
+		return NULL;
+	}
+	char *ptr = s;
+	
+	while (*ptr != '\0')
+	{
+	if (*ptr == c)
+	{
+		return ptr;
+	}
+	ptr++;
+	}
+	return NULL;
+}
