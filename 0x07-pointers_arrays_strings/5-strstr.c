565b0c9c8a72751b6330bcd2d38d49cd699db619
gouzidev <gouzi.dev@gmail.com>
2023-04-03 18:08:45 -0700

    new

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,9 +11,9 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i = 0;
 	int j = 0;
-	
+
 	if (*needle == '\0')
-		return haystack;
+		return (haystack);
 
 	while (*(haystack + i) != '\0')
 	{
