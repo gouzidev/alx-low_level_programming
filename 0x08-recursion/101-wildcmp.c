ae59979d09ad86b96abdf241ff9f25a1f2746b87
gouzidev <gouzi.dev@gmail.com>
2023-04-05 17:08:47 -0700

    new

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* wildcmp - Entry point
+* @s1: str 1
+* @s2: str 2
+* Return: 0
+*/
+
+int wildcmp(char *str1, char *str2)
+{
+if (*str1 == '\0' && *str2 == '\0')
+return 1;
+if (*str2 == *str1)
+return wildcmp(str1 + 1, str2 + 1);
+if (*str2 == '\0')
+return 0;
+if (*str1 == '\0' && *str2 != '*')
+return 0;
+if (*str1 == '\0' && *str2 == '*')
+return wildcmp(str1, str2 + 1);
+if (*str2 == '*')
+return wildcmp(str1, str2 + 1) || wildcmp(str1 + 1, str2);
+return 0;
+}
