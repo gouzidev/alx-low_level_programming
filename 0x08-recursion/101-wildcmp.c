dab82009dcb91d95e3b434d190c1b2188a97ecb7
gouzidev <gouzi.dev@gmail.com>
2023-04-05 17:09:22 -0700

    new

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,16 +11,16 @@
 int wildcmp(char *str1, char *str2)
 {
 if (*str1 == '\0' && *str2 == '\0')
-return 1;
+return (1);
 if (*str2 == *str1)
 return wildcmp(str1 + 1, str2 + 1);
 if (*str2 == '\0')
-return 0;
+return (0);
 if (*str1 == '\0' && *str2 != '*')
-return 0;
+return (0);
 if (*str1 == '\0' && *str2 == '*')
 return wildcmp(str1, str2 + 1);
 if (*str2 == '*')
 return wildcmp(str1, str2 + 1) || wildcmp(str1 + 1, str2);
-return 0;
+return (0);
 }
