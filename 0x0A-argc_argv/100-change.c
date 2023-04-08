8d8ae9ba8dba52ed99fef0eb8b67ec538d321e26
gouzidev <gouzi.dev@gmail.com>
2023-04-07 18:01:39 -0700

    new

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+int n;
+int sum = 0;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+
+n = atoi(argv[argc - 1]);
+
+if (n >= 25)
+{
+sum += n / 25;
+n = n % 25;
+}
+if (n >= 10)
+{
+sum += n / 10;
+n = n % 10;
+}
+
+if (n >= 5)
+{
+sum += n / 5;
+n = n % 5;
+}
+if (n >= 2)
+{
+sum += n / 2;
+n = n % 2;
+}
+sum += n;
+printf("%d",sum);
+return (0);
+}
