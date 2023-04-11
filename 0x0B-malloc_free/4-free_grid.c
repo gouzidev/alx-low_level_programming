b4ad99005d3cbc38dbf4001d6d1fe7cbf695ef15
gouzidev <gouzi.dev@gmail.com>
2023-04-11 16:22:28 -0700

    new

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - allocates a arr, make space and free space
+ * @grid: test
+ * @height: height of arr
+ * Return: arr with freed spaces
+ */
+
+void free_grid(int **grid, int height)
+{
+int i = 0;
+while (i < height)
+{
+free(grid[i]);
+}
+free(grid);
+}
