b8406473d70df123856e63e4d7d2089d090abdde
gouzidev <gouzi.dev@gmail.com>
2023-04-11 16:40:11 -0700

    new

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ if (width * height <= 0)
 return (NULL);
 }
 
-arr = (int**) malloc(sizeof(int *) * height);
+arr = (int **) malloc(sizeof(int *) * height);
 
 if (arr == NULL)
 {
@@ -27,7 +27,7 @@ return (NULL);
 
 for (i = 0; i < height; i++)
 {
-arr[i] = (int*) malloc(sizeof(int) * width);
+arr[i] = (int *) malloc(sizeof(int) * width);
 if (arr[i] == NULL)
 {
 for (i = i - 1; i >= 0; i--)
