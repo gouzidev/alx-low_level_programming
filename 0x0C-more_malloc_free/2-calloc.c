061f00521260ff64e2530bcd0caca0b5426960a3
gouzidev <gouzi.dev@gmail.com>
2023-04-13 20:14:54 -0700

    new

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,44 +1,40 @@
 #include "main.h"
 
 /**
- * _calloc - array of elements, each of size bytes.
- * @nmemb: Number of elements to allocate memory for.
- * @size: Size of each element in bytes.
- *
- * Return: If nmemb or size is 0, or if malloc fails - NULL.
- *         Otherwise - a pointer to the allocated memory.
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements of pointer
+ * @size: size of each member
+ * Return: pointer of allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *allocated_memory;
+void *p;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-
-	allocated_memory = malloc(size * nmemb);
-
-	if (allocated_memory == NULL)
-		return (NULL);
-
-	_memset(allocated_memory, 0, size * nmemb);
-
-	return (allocated_memory);
+if (!nmemb || !size)
+return (NULL);
+p = malloc(size * nmemb);
+if (!p)
+{
+return (NULL);
+}
+_memset(p, 0, (nmemb * size));
+return (p);
 }
 
 /**
- * _memset - Fills the first n bytes.
- * @s: Pointer to the memory area to be filled.
- * @b: The constant byte to fill the memory with.
- * @n: The number of bytes to be filled.
- *
- * Return: A pointer to the memory area s.
+ * _memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: bytes of the memory area
+ * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *p = s;
+char *p = s;
 
-	while (n--)
-		*p++ = b;
-
-	return (s);
+while (n)
+*s = b;
+s++;
+n--;
+return (p);
 }
