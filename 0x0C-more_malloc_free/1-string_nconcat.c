a216942a1bfbe88b364afae2aba016b7c444d09d
gouzidev <gouzi.dev@gmail.com>
2023-04-13 20:06:56 -0700

    new

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,52 +2,43 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - Concatinate first string
- * and n chars from the second string.
+ * string_nconcat - Concatenates two strings using at
+ *                  most an inputted number of bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
  *
- * @s1: First string .
- * @s2: Second string.
- * @n: Numbers of chars to concatinate from the second string.
- * Return: Pointer to the new string.
- **/
-
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the concatenated space in memory.
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int s1_length = 0, s2_length = 0, k = 0;
+	char *concatenated_string;
+	unsigned int concatenated_string_length = n, index;
 
-	if (s1 != NULL)
-		while (str1[str1_len] != '\0')
-		{
-			str1_len++;
-		}
-	if (s2 != NULL)
-		while (str2[str2_len] != '\0')
-		{
-			str2_len++;
-		}
-	if (n >= s2_length)
-		s = malloc(sizeof(char) * (s1_length + s2_length + 1));
-	else
-	{
-		s = malloc(sizeof(char) * (s1_length + n + 1));
-		s2_length = n;
-	}
-	if (s == NULL)
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index]; index++)
+		concatenated_string_length++;
+
+	concatenated_string = malloc(sizeof(char) * (concatenated_string_length + 1));
+
+	if (concatenated_string == NULL)
 		return (NULL);
 
-	while (k < s1_length)
-	{
-		s[k] = s1[k];
-		k++;
-	}
+	concatenated_string_length = 0;
 
-	while (k < s1_length + s2_length)
-	{
-		s[k] = s2[k - s1_length];
-		k++;
-	}
-	s[k] = '\0';
-	return (s);
+	for (index = 0; s1[index]; index++)
+		concatenated_string[concatenated_string_length++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concatenated_string[concatenated_string_length++] = s2[index];
+
+	concatenated_string[concatenated_string_length] = '\0';
+
+	return (concatenated_string);
 }
-
