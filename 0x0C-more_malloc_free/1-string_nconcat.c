dcf15e62a92b28384ee6bc13a8b056bb8fd111d6
gouzidev <gouzi.dev@gmail.com>
2023-04-13 19:55:35 -0700

    new

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - Concatenate first string and n chars from the second string.
+ * string_nconcat - Cstring and n chars from the second string.
  *
  * @str1: First string.
  * @str2: Second string.
@@ -17,49 +17,28 @@ unsigned int str2_len = 0;
 unsigned int i = 0;
 
 if (str1 != NULL)
-{
-while (str1[str1_len] != '\0')
-{
-str1_len++;
-}
-}
-
+	while (str1[str1_len] != '\0')
+		str1_len++;
 if (str2 != NULL)
-{
-while (str2[str2_len] != '\0')
-{
-str2_len++;
-}
-}
-
+	while (str2[str2_len] != '\0')
+		str2_len++;
 if (n >= str2_len)
-{
-result = malloc(sizeof(char) * (str1_len + str2_len + 1));
-}
+	result = malloc(sizeof(char) * (str1_len + str2_len + 1));
 else
-{
-result = malloc(sizeof(char) * (str1_len + n + 1));
+	result = malloc(sizeof(char) * (str1_len + n + 1));
 str2_len = n;
-}
-
 if (result == NULL)
-{
 return (NULL);
-}
-
 while (i < str1_len)
 {
 result[i] = str1[i];
 i++;
 }
-
 while (i < str1_len + str2_len)
 {
 result[i] = str2[i - str1_len];
 i++;
 }
-
 result[i] = '\0';
-
 return (result);
 }
