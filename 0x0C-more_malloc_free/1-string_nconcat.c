486b079f30e98c7aa48ec07029808b53d2fc1a7f
gouzidev <gouzi.dev@gmail.com>
2023-04-13 19:52:11 -0700

    new

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,48 +2,64 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - Concatinate first string
- * and n chars from the second string.
+ * string_nconcat - Concatenate first string and n chars from the second string.
  *
- * @s1: First string .
- * @s2: Second string.
- * @n: Numbers of chars to concatinate from the second string.
+ * @str1: First string.
+ * @str2: Second string.
+ * @n: Numbers of chars to concatenate from the second string.
  * Return: Pointer to the new string.
  **/
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *str1, char *str2, unsigned int n)
 {
-	char *s;
-	unsigned int s1_length = 0, s2_length = 0, k = 0;
+char *result;
+unsigned int str1_len = 0;
+unsigned int str2_len = 0;
+unsigned int i = 0;
 
-	if (s1 != NULL)
-		for (; s1[s1_length]; s1_length++)
-		;
-	if (s2 != NULL)
-		for (; s2[s2_length]; s2_length++)
-		;
-	if (n >= s2_length)
-		s = malloc(sizeof(char) * (s1_length + s2_length + 1));
-	else
-	{
-		s = malloc(sizeof(char) * (s1_length + n + 1));
-		s2_length = n;
-	}
-	if (s == NULL)
-		return (NULL);
-
-	while (k < s1_length)
-	{
-		s[k] = s1[k];
-		k++;
-	}
-
-	while (k < s1_length + s2_length)
-	{
-		s[k] = s2[k - s1_length];
-		k++;
-	}
-	s[k] = '\0';
-	return (s);
+if (str1 != NULL)
+{
+while (str1[str1_len] != '\0')
+{
+str1_len++;
+}
 }
 
+if (str2 != NULL)
+{
+while (str2[str2_len] != '\0')
+{
+str2_len++;
+}
+}
+
+if (n >= str2_len)
+{
+result = malloc(sizeof(char) * (str1_len + str2_len + 1));
+}
+else
+{
+result = malloc(sizeof(char) * (str1_len + n + 1));
+str2_len = n;
+}
+
+if (result == NULL)
+{
+return (NULL);
+}
+
+while (i < str1_len)
+{
+result[i] = str1[i];
+i++;
+}
+
+while (i < str1_len + str2_len)
+{
+result[i] = str2[i - str1_len];
+i++;
+}
+
+result[i] = '\0';
+
+return (result);
+}
