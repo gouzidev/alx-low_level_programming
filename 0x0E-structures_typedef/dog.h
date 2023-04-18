fef7a0f154f59d9ffc290f549460c9b49749a503
gouzidev <gouzi.dev@gmail.com>
2023-04-17 19:10:30 -0700

    new

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,13 +3,7 @@
 #define STRUCT_DOG
 
 /**
- * dog - idk
- */
-
-typedef struct dog dog;
-
-/**
- * dog - Short description
+ * struct dog - Short description
  * @name: First member
  * @age: Second member
  * @owner: Third member
@@ -17,11 +11,12 @@ typedef struct dog dog;
  * Description: Longer description
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
-	char *owner
+	char *owner;
 }
+	dog;
 
 #endif
