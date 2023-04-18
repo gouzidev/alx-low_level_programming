c3e84fa510260ebacd54908042bdb0d05724b487
gouzidev <gouzi.dev@gmail.com>
2023-04-17 19:20:26 -0700

    new

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,4 +19,14 @@ typedef struct dog
 }
 	dog;
 
+/*task three, new type dog_t as new name of strcut dog */
+typedef struct dog dog_t;
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
 #endif
