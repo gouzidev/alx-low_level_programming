e54d38c556d76946004521e6de162b7ad3d567f7
gouzidev <gouzi.dev@gmail.com>
2023-04-17 19:35:21 -0700

    new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,30 +3,23 @@
 #include "dog.h"
 /**
  * new_dog - creates new dog
- *
  * @name: pointer to copy
  * @owner: pointer to copy
  * @age: float  type
- *
  * Return: to dog_t or NULL if it fail
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int name_r = 0;
-int owner_r = 0;
+int name_r = 0, owner_r = 0;
 dog_t *new_dog = malloc(sizeof(dog_t));
 
 if (new_dog == NULL)
-{
 return (new_dog);
-}
 while (name[name_r])
-{
 name_r++;
-}
 while (owner[owner_r])
 {
-owner_r++;
+	owner_r++;
 }
 new_dog->name =  malloc(sizeof(char) * (name_r + 1));
 new_dog->owner = malloc(sizeof(char) * (owner_r + 1));
