c6196bc5b58ed0968ce91d33c88eaa0534a9ff74
gouzidev <gouzi.dev@gmail.com>
2023-04-17 19:40:45 -0700

    new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,39 +3,49 @@
 #include "dog.h"
 /**
  * new_dog - creates new dog
+ *
  * @name: pointer to copy
  * @owner: pointer to copy
  * @age: float  type
+ *
  * Return: to dog_t or NULL if it fail
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int namerunner, ownerrunner;
-	dog_t *new_dog = malloc(sizeof(dog_t));
+int name_r, owner_r;
+dog_t *new_dog = malloc(sizeof(dog_t));
 
-	if (new_dog == NULL)
-		return (new_dog);
-	for (namerunner = 0; name[namerunner]; namerunner++)
-	{
-	}
-	for (ownerrunner = 0; owner[ownerrunner]; ownerrunner++)
-	{
-	}
-	new_dog->name = malloc((namerunner + 1) * sizeof(char));
-	new_dog->owner = malloc((ownerrunner + 1) * sizeof(char));
-	if (new_dog->name == NULL || new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog->owner);
-		free(new_dog);
-		return (NULL);
-	}
-	for (namerunner = 0; name[namerunner]; namerunner++)
-		new_dog->name[namerunner] = name[namerunner];
-	for (ownerrunner = 0; owner[ownerrunner]; ownerrunner++)
-		new_dog->owner[ownerrunner] = owner[ownerrunner];
-	new_dog->name[namerunner] = 0;
-	new_dog->owner[ownerrunner] = 0;
-	new_dog->age = age;
-	return (new_dog);
+if (new_dog == NULL)
+return (new_dog);
+name_r = 0;
+while (name[name_r]) {
+name_r++;
+}
+owner_r = 0;
+while (owner[owner_r]) {
+owner_r++;
+}
+new_dog->name = malloc((name_r + 1) * sizeof(char));
+new_dog->owner = malloc((owner_r + 1) * sizeof(char));
+if (new_dog->name == NULL || new_dog->owner == NULL)
+{
+free(new_dog->name);
+free(new_dog->owner);
+free(new_dog);
+return (NULL);
+}
+name_r = 0;
+while (name[name_r]) {
+new_dog->name[name_r] = name[name_r];
+name_r++;
+}
+owner_r = 0;
+while (owner[owner_r]) {
+new_dog->owner[owner_r] = owner[owner_r];
+owner_r++;
+}
+new_dog->name[name_r] = 0;
+new_dog->owner[owner_r] = 0;
+new_dog->age = age;
+return (new_dog);
 }
