922551380dc5354bc958fd5287dcb260fff7742f
gouzidev <gouzi.dev@gmail.com>
2023-04-19 08:21:54 -0700

    new

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,16 +1,27 @@
+/**
+ * int_index - searches for an integer
+ *
+ * @array: pointer to an int array
+ * @size: number of elements in the array 'array'
+ * @cmp: pointer to a function that receives an int
+ *
+ * Return: int
+ */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
 	if (size <= 0)
 	{
-		return -1
+		return (-1);
 	}
 
 	while (i < size)
 	{
 		if (cmp(array[i])
-			return i;
+			return (i);
 		i++;
 	}
-	return -1;
+	return (-1);
+}
