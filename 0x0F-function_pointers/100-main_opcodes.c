b3bd3c1a6e2dc16cbc00253d86181281bfbcad54
gouzidev <gouzi.dev@gmail.com>
2023-04-19 11:03:49 -0700

    new

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - prints the opcodes of its own main function
+ *
+ * @argv: the argument that enters to the program
+ * @argc: counts the number of arguments
+ *
+ * Return: the opcode hex number
+ */
+
+int main(char argc, char *argv[])
+{
+int n_bytes = atoi(argv[1]);
+int i = 0;
+
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+if (n_bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+while (i < n_bytes)
+{
+printf("%02hhx", main + i);
+i++;
+if (i != n_bytes)
+{
+printf(" ");
+}
+}
+printf("\n");
+return (0);
+}
