43612a5b7551087a550240c311b288e0282c2e17
gouzidev <gouzi.dev@gmail.com>
2023-04-19 07:58:32 -0700

    new

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ *
+ * @name: pointer to a char
+ * @f: a pointer to function that receives a pointer to char
+ *
+ * Return: void or NULL
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
