08b2f8f3c2bdf6027b4d334c8409d9496507ee73
gouzidev <gouzi.dev@gmail.com>
2023-04-27 11:27:27 -0700

    new

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * before_main - frees list_t
+ * Return: void
+ */
+
+
+void __attribute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
