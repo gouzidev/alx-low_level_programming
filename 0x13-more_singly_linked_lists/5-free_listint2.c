fb8a929ed22feb06d2e1d90da7165690e48ed8d1
gouzidev <gouzi.dev@gmail.com>
2023-05-01 01:01:03 -0700

    new

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * free_listint2 - adds a new node at the end of a list_t list
+ * @head: double pointer to listint_t
+ * Return: he address of the new element, or NULL if it failed
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *curr = *head;
+listint_t *next = *head;
+if (*head == NULL)
+{
+free(*head);
+return;
+}
+else
+{
+while (curr->next)
+{
+next = curr->next;
+free(curr);
+curr = next;
+}
+free(curr);
+}
+*head = NULL;
+}
