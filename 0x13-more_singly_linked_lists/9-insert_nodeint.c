97edf7dd48849da573b6eef7476eec3753cf7f42
gouzidev <gouzi.dev@gmail.com>
2023-05-01 04:10:00 -0700

    new

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,30 +10,37 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+listint_t *curr;
+listint_t *new_node;
 unsigned int counter;
-listint_t *new_node = malloc(sizeof(listint_t));
-listint_t *curr = *head;
 
+counter = 0;
+/* allocate memory for the new_node */
+new_node = malloc(sizeof(listint_t));
+/* task condition */
 if (new_node == NULL)
 return (NULL);
-
+/* n keeps being the data of the new node */
 new_node->n = n;
-counter = 0;
-
-/* if idx = 0 */
+/*  given the case, if index is cero then new_node is equal to head */
+/* and return new_node in head of the list */
 if (idx == 0)
 {
 new_node->next = *head;
 *head = new_node;
 return (new_node);
 }
-/* else */
+/* usea de auxiliar */
+curr = *head;
+/* if index is different to the position */
+/* counter counter keeps running and curr moves to the next node */
 while (curr != NULL && counter != idx - 1)
 {
 counter++;
 curr = curr->next;
 }
-if (counter == idx)
+/* if index and counter are equal and non NULL */
+if (counter == idx - 1 && curr != NULL)
 {
 new_node->next = curr->next;
 curr->next = new_node;
