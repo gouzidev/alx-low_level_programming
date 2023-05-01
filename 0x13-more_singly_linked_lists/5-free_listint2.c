50e14031b054f2054e47dd1125cdac9557c8eb4d
gouzidev <gouzi.dev@gmail.com>
2023-05-01 01:13:19 -0700

    new

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,19 +6,26 @@
  * @head: double pointer to listint_t
  * Return: he address of the new element, or NULL if it failed
  */
+
 void free_listint2(listint_t **head)
 {
-listint_t *temp;
-
-if (head == NULL || *head == NULL)
-return;
-
-while (*head != NULL)
+listint_t *curr = *head;
+listint_t *next = *head;
+if (*head == NULL || head == NULL)
 {
-temp = *head;
-*head = (*head)->next;
-free(temp);
+free(head);
+*head = NULL;
+return;
 }
-
+else
+{
+while (curr->next)
+{
+next = curr->next;
+free(curr);
+curr = next;
+}
+free(curr);
+}
 *head = NULL;
 }
