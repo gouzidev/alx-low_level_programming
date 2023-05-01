fd3f009df2fd11f8cd85a895cdb1a9ec4fbdf1a3
gouzidev <gouzi.dev@gmail.com>
2023-05-01 02:17:02 -0700

    new

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,18 +10,17 @@ int pop_listint(listint_t **head)
 {
 	int n = 0;
 	listint_t *ptraux;
-	/* task condition */
+
 	if (*head == NULL)
-		return (0);
-	/* n is equal to the head de reference to n from the structure */
-	/* that is the nodes data (n) that are integers */
+		return (n);
+
 	n = (*head)->n;
-	/* head goes to the next node */
+
 	ptraux = (*head)->next;
-	/* frees head so the head node is deleted */
+
 	free(*head);
-	/* now ptraux is the new head node */
+
 	*head = ptraux;
-	/* return the head nodes data that is n according to the structure */
+
 	return (n);
 }
