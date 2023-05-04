d7573231be66d9be6ef6a9f963532bb7bfefe72f
gouzidev <gouzi.dev@gmail.com>
2023-05-04 03:21:27 -0700

    new

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+if (n > 1)
+print_binary(n >> 1);
+if (n & 1)
+{
+putchar('1');
+}
+else {
+_putchar('0');
+}
+}
