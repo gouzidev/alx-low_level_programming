7bf84bec6a5589adf8a680f78e501f45e73d5841
gouzidev <gouzi.dev@gmail.com>
2023-05-04 03:47:04 -0700

    new

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * power - convert binary to decimal.
+ * @base: the binary number in string format
+ * @exponent: the binary number in string format
+ * Description: convert binary to decimal
+ * Return: this return the convert number.
+ */
+int power(int base, int exponent)
+{
+int result = 1;
+int i = 0;
+while (i < exponent)
+{
+result *= base;
+i++;
+}
+return (result);
+}
+
+/**
+ * get_bit - prints the binary representation of a number
+ * @n: the number to print in binary
+ * @index: the number to print in binary
+ * Return: this return the convert number.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+return (n & power(2, index) ? 1 : 0);
+}
