df83f66f3af6a79c4b0fe20723661dfe36ab0995
gouzidev <gouzi.dev@gmail.com>
2023-05-04 04:31:58 -0700

    new

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,20 +1,36 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * power - convert binary to decimal.
+ * @base: the binary number in string format
+ * @exponent: the binary number in string format
+ * Description: convert binary to decimal
+ * Return: this return the convert number.
+ */
+int power(int base, int exponent)
+{
+int result = 1;
+int i = 0;
+while (i < exponent)
+{
+result *= base;
+i++;
+}
+return (result);
+}
 
 /**
- * clear_bit - prints the binary representation of a number
+ * set_bit - prints the binary representation of a number
  * @n: the number to print in binary
  * @index: the number to print in binary
  * Return: this return the convert number.
  */
-int clear_bit(unsigned long int *n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-unsigned int mask;
-
 if (index > 64)
+{
 return (-1);
-mask = 1 << index;
-mask = ~mask;
-*n = (*n & mask);
+}
+*n = (*n | power(2, index));
 return (1);
 }
