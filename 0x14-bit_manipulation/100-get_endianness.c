7cc6bfa3fa0979d799c7a0cde9cad247e55ca5c1
gouzidev <gouzi.dev@gmail.com>
2023-05-04 17:24:20 -0700

    new

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,10 +1,8 @@
-#include "main.h"
 #include <stdint.h>
 
 /**
- * get_endianness - rwould need to flip to get from
- *             one number to another.
- * Return: the number of bits needed to flip
+ * get_endianness - checks if a machine is little or big endian
+ * Return: 0 for big, 1 for little
  */
 
 int get_endianness(void)
@@ -12,8 +10,7 @@ int get_endianness(void)
 unsigned int value;
 uint8_t *ptr;
 
-value =  = 305419896;
 ptr = (uint8_t *)&value;
-
+value = 305419896;
 return ((*ptr == 0x78) ? 1 : 0);
 }
