5662610078e89ec8756c2b3435da2d412ef07ac7
gouzidev <gouzi.dev@gmail.com>
2023-05-08 13:57:58 -0700

    new

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,19 +13,19 @@ char *buffer;
 
 if (argc != 3)
 {
-dprintf(2, "Usage: cp argv[1] argv[2]\n");
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 file_from_fd = open(argv[1], O_RDONLY);
 if (file_from_fd == -1)
 {
-dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-file_to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 661);
+file_to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (file_to_fd == -1)
 {
-dprintf(2, "Error: Can't write to %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 buffer = malloc(sizeof(char) * 1024);
@@ -35,14 +35,14 @@ bytes_written = write(file_to_fd, buffer, bytes_read);
 if (bytes_read != bytes_written)
 return (-1);
 }
-if (close(file_from_fd) == -1)
+if (close(file_from_fd) < 0)
 {
-dprintf(2, "Error: Can't close fd %d\n", file_from_fd);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from_fd);
 exit(100);
 }
-if (close(file_to_fd) == -1)
+if (close(file_to_fd) < 0)
 {
-dprintf(2, "Error: Can't close fd %d\n", file_to_fd);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to_fd);
 exit(100);
 }
 free(buffer);
