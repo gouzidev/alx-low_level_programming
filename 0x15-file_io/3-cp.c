64603ab882b2865234b1fb162b457823a8dcdda2
gouzidev <gouzi.dev@gmail.com>
2023-05-08 13:45:17 -0700

    new

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * main - copy a file.
+ * @argc: the number of args
+ * @argv: the Args.
+ * Description: copy a file
+ * Return: 0 in success.
+ */
+int main(int argc, char *argv[])
+{
+int file_to_fd, file_from_fd, bytes_read, bytes_written;
+char *buffer;
+
+if (argc != 3)
+{
+dprint(2, "Usage: cp argv[1] argv[2]\n");
+exit(97);
+}
+file_from_fd = open(argv[1], O_RDONLY);
+if (file_from_fd == -1)
+{
+dprint(2, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+file_to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 661);
+if (file_to_fd == -1)
+{
+dprint(2, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+buffer = malloc(sizeof(char) * 1024);
+while ((bytes_read = read(file_from_fd, buffer, 1024)) > 0)
+{
+bytes_written = write(file_to_fd, buffer, bytes_read);
+if (bytes_read != bytes_written)
+return (-1);
+}
+if (close(file_from_fd) == -1)
+{
+dprint(2, "Error: Can't close fd %d\n", file_from_fd);
+exit(100);
+}
+if (close(file_to_fd) == -1)
+{
+dprint(2, "Error: Can't close fd %d\n", file_to_fd);
+exit(100);
+}
+free(buffer);
+return (0);
+}
