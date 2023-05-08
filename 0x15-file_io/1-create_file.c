b1a54ce70e0a0e4f993a9efbef65e69f97e54df6
gouzidev <gouzi.dev@gmail.com>
2023-05-08 09:22:08 -0700

    new

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *  create_file - create a file and write in it
+ * @filename:file to be created
+ * @text_content: content of the file created
+ * Return: 1 or -1;
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fh;
+int _write_flag;
+
+if (filename == NULL)
+{
+return (-1);
+}
+
+fh = open(filename, O_CREAT | O_WRONLY);
+
+if (fh == -1)
+{
+return (-1);
+}
+
+_write_flag = write(fh, text_content, sizeof(text_content));
+
+if (_write_flag == -1)
+{
+return (-1);
+}
+return (1);
+}
