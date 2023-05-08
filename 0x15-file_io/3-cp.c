eae5806ef00b2945d1d9c2b549ca80569cf36eb3
gouzidev <gouzi.dev@gmail.com>
2023-05-08 13:48:38 -0700

    new

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,19 +13,19 @@ char *buffer;
 
 if (argc != 3)
 {
-dprint(2, "Usage: cp argv[1] argv[2]\n");
+dprintf(2, "Usage: cp argv[1] argv[2]\n");
 exit(97);
 }
 file_from_fd = open(argv[1], O_RDONLY);
 if (file_from_fd == -1)
 {
-dprint(2, "Error: Can't read from file %s\n", argv[1]);
+dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 file_to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 661);
 if (file_to_fd == -1)
 {
-dprint(2, "Error: Can't write to %s\n", argv[2]);
+dprintf(2, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 buffer = malloc(sizeof(char) * 1024);
@@ -37,12 +37,12 @@ return (-1);
 }
 if (close(file_from_fd) == -1)
 {
-dprint(2, "Error: Can't close fd %d\n", file_from_fd);
+dprintf(2, "Error: Can't close fd %d\n", file_from_fd);
 exit(100);
 }
 if (close(file_to_fd) == -1)
 {
-dprint(2, "Error: Can't close fd %d\n", file_to_fd);
+dprintf(2, "Error: Can't close fd %d\n", file_to_fd);
 exit(100);
 }
 free(buffer);
