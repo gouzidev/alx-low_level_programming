5d2f7dcdfe93aac9e30860d3799c983dad6a59b5
gouzidev <gouzi.dev@gmail.com>
2023-05-08 14:06:55 -0700

    new

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - copy a file.
  * @argc: the number of args
@@ -6,9 +7,10 @@
  * Description: copy a file
  * Return: 0 in success.
  */
+
 int main(int argc, char *argv[])
 {
-int file_to_fd, file_from_fd, bytes_read, bytes_written;
+int file_to_fd, file_from_fd, bytes_read;
 char *buffer;
 
 if (argc != 3)
@@ -31,20 +33,15 @@ exit(99);
 buffer = malloc(sizeof(char) * 1024);
 while ((bytes_read = read(file_from_fd, buffer, 1024)) > 0)
 {
-bytes_written = write(file_to_fd, buffer, bytes_read);
-if (bytes_read != bytes_written)
-return (-1);
+if (file_to_fd == -1 || (write(file_to_fd, buffer, bytes_read) != bytes_read))
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),exit(99);
 }
+if (bytes_read < 0)
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),exit(98);
 if (close(file_from_fd) < 0)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from_fd);
-exit(100);
-}
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from_fd),exit(100);
 if (close(file_to_fd) < 0)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to_fd);
-exit(100);
-}
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to_fd),exit(100);
 free(buffer);
 return (0);
 }
