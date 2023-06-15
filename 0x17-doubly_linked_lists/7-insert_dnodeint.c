ee437c9d89f53c019acfb6bf994832cc6c870357
gouzidev <gouzi.dev@gmail.com>
2023-06-16 00:19:36 +0100

    commit

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,63 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserrtt
+ * @h: head
+ * @idx: idx
+ * @n: n
+ *
+ * Return: adrr
+ *
+ */
+
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+unsigned int i = 0;
+dlistint_t *curr = *h;
+dlistint_t *next = *h;
+
+dlistint_t *new = malloc(sizeof(dlistint_t));
+
+
+if (new == NULL)
+return NULL;
+
+new->n = n;
+new->next = NULL;
+new->prev = NULL;
+
+while (curr)
+{
+if (idx == 0 && i == 0)
+{
+curr->prev = new;
+new->next = curr;
+new->prev = curr->prev;
+*h = new;
+return (new);
+}
+if (i == idx - 1)
+{
+if (!curr->next)
+{
+curr->next = new;
+new->prev = curr;
+return (new);
+}
+else
+{
+next = curr->next;
+new->prev = curr;
+new->next = next;
+curr->next = new;
+next->prev = new;
+}
+return (new);
+}
+curr = curr->next;
+i++;
+}
+
+return (NULL);
+}
