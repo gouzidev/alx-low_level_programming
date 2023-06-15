a49d32127a4359f53ee19bd387c7dcd1f9f7425a
gouzidev <gouzi.dev@gmail.com>
2023-06-15 22:01:11 +0100

    commit

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add node dbl linked list int
+ * @head: head
+ * @n: val of node
+ *
+ * Return: adrr
+ *
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+dlistint_t *new = malloc(sizeof(dlistint_t));
+dlistint_t *curr = *head;
+
+new->n = n;
+
+if (*head == NULL)
+{
+*head = new;
+(*head)->next = NULL;
+(*head)->prev = NULL;
+}
+else if ((*head)->next == NULL)
+{
+(*head)->next = new;
+new->next = NULL;
+new->prev = *head;
+}
+else
+{
+while ((*head)->next)
+{
+*head = (*head)->next;
+}
+(*head)->next = new;
+new->prev = *head;
+*head = curr;
+}
+
+return (new);
+}
