425c0da0fc1999797a75175d8bcebeb0f45117ed
gouzidev <gouzi.dev@gmail.com>
2023-06-15 18:03:38 +0100

    commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,12 +11,11 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    return (NULL);
-    new->prev = NULL;
-    new->n = n;
-    new->next = *head;
-    *head = new;
-    return (new);
+dlistint_t *new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = *head;
+*head = new;
+return (new);
 }
