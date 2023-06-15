bcad756b45b7bec7e6b50575e0d1388d2cdc9b3e
gouzidev <gouzi.dev@gmail.com>
2023-06-15 22:52:25 +0100

    commit

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,22 +12,22 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 dlistint_t *new = malloc(sizeof(dlistint_t));
-dlistint_t *curr;
-new->n = n;
+dlistint_t *curr = *head;
+new->next = NULL;
+new->prev = NULL;
 
 if (new == NULL)
 return (NULL);
 
-else
-{
 if (*head == NULL)
 {
-new->next = NULL;
-new->prev = NULL;
+new->n = n;
 *head = new;
+return (new);
 }
 else
 {
+new->n = n;
 curr = *head;
 while (curr->next != NULL)
 {
@@ -36,7 +36,6 @@ curr = curr->next;
 curr->next = new;
 new->prev = curr;
 new->next = NULL;
-}
 }
 return (new);
 }
