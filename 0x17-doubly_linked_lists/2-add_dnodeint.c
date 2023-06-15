51a1a5140b284e5127240e5d2f87b4bf9adec4bd
gouzidev <gouzi.dev@gmail.com>
2023-06-15 17:41:17 +0100

    commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,12 +1,22 @@
 #include "lists.h"
 
+/**
+ * add_dnodeint - add node dbl linked list int
+ * @head: head
+ * @n: val of node
+ *
+ * Return: adrr
+ *
+ */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *new = malloc(sizeof(dlistint_t));
-if (new == NULL) return (NULL);
+if (new == NULL)
+return (NULL);
 new->prev = NULL;
 new->n = n;
 new->next = *head;
 *head = new;
-return new;
+return (new);
 }
