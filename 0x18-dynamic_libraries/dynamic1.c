e2c00260ad64a8e320d1b49ec59fec3b8f871ea2
gouzidev <gouzi.dev@gmail.com>
2023-06-20 00:53:28 +0100

    commit

---
+++ 0x18-dynamic_libraries/dynamic1.c
@@ -0,0 +1,50 @@
+/**
+ * _putchar - prints char
+ * @c: char to print
+ * Return: always 1
+ */
+
+int _putchar(char c)
+{
+return (1);
+}
+/**
+ * _islower - prints char
+ * @c: char to print
+ * Return: always 1
+ */
+
+int _islower(int c)
+{
+return (1);
+}
+/**
+ * _isalpha - prints char
+ * @c: char to print
+ * Return: always 1
+ */
+
+int _isalpha(int c)
+{
+return (1);
+}
+/**
+ * _abs - prints char
+ * @n: char to print
+ * Return: always 1
+ */
+
+int _abs(int n)
+{
+return (1);
+}
+/**
+ * _isupper - prints char
+ * @c: char to print
+ * Return: always 1
+ */
+
+int _isupper(int c)
+{
+return (1);
+}
---
+++ 0x18-dynamic_libraries/dynamic3.c
@@ -0,0 +1,55 @@
+/**
+ * _atoi - prints char
+ * @s: char to print
+ * Return: always 1
+ */
+int _atoi(char *s)
+{
+return (1);
+}
+/**
+ * *_strcat - prints char
+ * @dest: char to print
+ * @src: char to print
+ * Return: always 1
+ */
+
+char *_strcat(char *dest, char *src)
+{
+return (1);
+}
+/**
+ * *_strncat - prints char
+ * @dest: char to print
+ * @src: char to print
+ * @n: blabla
+ * Return: always 1
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+return (1);
+}
+/**
+ * *_strncpy - prints char
+ * @dest: char to print
+ * @src: char to print
+ * @n: something
+ * Return: always 1
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+return (1);
+}
+/**
+ * _strcmp - prints char
+ * @s1: char to print
+ * @s2: something
+ * Return: always 1
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+return (1);
+}
