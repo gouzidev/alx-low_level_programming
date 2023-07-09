c96fa15c76c8a775589de66997bac8488f6f8297
gouzidev <gouzi.dev@gmail.com>
2023-07-09 12:04:24 +0100

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,12 +9,12 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    ht->size = size;
     hash_table_t *ht = malloc(sizeof(hash_node_t));
     if (ht == NULL)
     {
         return (NULL);
     }
+    ht->size = size;
     ht->array = malloc(sizeof(hash_node_t) * size);
     if (ht->array == NULL)
     {
