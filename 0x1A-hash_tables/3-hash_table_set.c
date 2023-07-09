04411504a104f34e5add65519bee20e8419c5fc5
gouzidev <gouzi.dev@gmail.com>
2023-07-09 16:01:39 +0100

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,12 +23,9 @@ if (new_node == NULL)
 return (0);
 
 new_node->key = malloc(sizeof(new_node->key));
-if (new_node->key == NULL)
-return (0);
 new_node->value = malloc(sizeof(new_node->value));
-if (new_node->value == NULL)
-return (0);
 new_node->next = malloc(sizeof(new_node->next));
+
 strcpy(new_node->key, key);
 strcpy(new_node->value, value);
 
@@ -36,17 +33,14 @@ existing_node = malloc(sizeof(hash_node_t));
 if (existing_node == NULL)
 return (0);
 
-hash_idx = key_index((const unsigned char *) key, ht->size);
+hash_idx = key_index((const unsigned char*) key, ht->size);
 existing_node = ht->array[hash_idx];
 if (!existing_node)
 ht->array[hash_idx] = new_node;
 else
 {
-while (existing_node->next)
-{
-existing_node = existing_node->next;
-}
 existing_node->next = new_node;
+existing_node = new_node;
 }
 new_node->next = NULL;
 return (1);
