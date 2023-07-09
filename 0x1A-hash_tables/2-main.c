6a60359b88d28f7b20618c1c61d6a374277b223e
gouzidev <gouzi.dev@gmail.com>
2023-07-09 13:27:39 +0100

    2

--- 0x1A-hash_tables/2-main.c
+++ 0x1A-hash_tables/2-main.c
@@ -1,3 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
 int main(void)
 {
     char *s;
