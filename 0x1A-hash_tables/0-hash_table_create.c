34be10e3a28c50bd6a4715ce77dd9eef2c2b05e7
gouzidev <gouzi.dev@gmail.com>
2023-07-09 12:05:30 +0100

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,13 +9,13 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *ht = malloc(sizeof(hash_node_t));
+hash_table_t *ht = malloc(sizeof(hash_node_t) + 1);
 if (ht == NULL)
 {
 return (NULL);
 }
 ht->size = size;
-ht->array = malloc(sizeof(hash_node_t) * size);
+ht->array = malloc((sizeof(hash_node_t) * size) + 1);
 if (ht->array == NULL)
 {
 return (NULL);
