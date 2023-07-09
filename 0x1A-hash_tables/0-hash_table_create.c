5071271bede61e5468a4dbaf573c34541c7d965d
gouzidev <gouzi.dev@gmail.com>
2023-07-09 12:02:51 +0100

    firstFile

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,17 +9,17 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *ht = malloc(sizeof(hash_node_t));
-if (ht == NULL)
-{
-return (NULL);
-}
-ht->size = size;
-ht->array = malloc(sizeof(hash_node_t) * size);
-if (ht->array == NULL)
-{
-return (NULL);
-}
-return (ht);
+    hash_table_t *ht = malloc(sizeof(hash_node_t));
+    if (ht == NULL)
+    {
+        return (NULL);
+    }
+    ht->array = malloc(sizeof(hash_node_t) * size);
+    if (ht->array == NULL)
+    {
+        return (NULL);
+    }
+    ht->size = size;
+    return (ht);
 }
 
