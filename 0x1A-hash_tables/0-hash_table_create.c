ad80fadd4004a38702c9f1cabe4ad21d29eb58ef
gouzidev <gouzi.dev@gmail.com>
2023-07-09 11:56:24 +0100

    firstFile

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,20 +1,25 @@
 #include "hash_tables.h"
 
-// void mallocate()
+/**
+ * hash_table_create - creates hash table
+ * @size: hash table size
+ * Return: hash table created
+ * Description: good smart function
+*/
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht = malloc(sizeof(hash_node_t));
-    if (ht == NULL)
-    {
-        return NULL;
-    }
-    ht->size = size;
-    ht->array = malloc(sizeof(hash_node_t) * size);
-    if (ht->array == NULL)
-    {
-        return NULL;
-    }
-    return ht;
+hash_table_t *ht = malloc(sizeof(hash_node_t));
+if (ht == NULL)
+{
+return (NULL);
+}
+ht->size = size;
+ht->array = malloc(sizeof(hash_node_t) * size);
+if (ht->array == NULL)
+{
+return (NULL);
+}
+return (ht);
 }
 
